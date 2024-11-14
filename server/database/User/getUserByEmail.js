9285bf4ee15dac119677be1da090c91f43191910
Kamit6337 <kamit6337@gmail.com>
2024-11-14 22:45:58 +0530

    update user in database

--- server/database/User/getUserByEmail.js
+++ server/database/User/getUserByEmail.js
@@ -1,16 +1,20 @@
 import User from "../../models/UserModel.js";
+import {
+  getUserByEmailRedis,
+  setUserIntoRedis,
+} from "../../redis/User/user.js";
 
 const getUserByEmail = async (email) => {
-  // const get = await getUserByEmailRedis(email);
+  const get = await getUserByEmailRedis(email);
 
-  // if (get) {
-  //   // Convert the plain object from Redis into a Mongoose document
-  //   return User.hydrate(get);
-  // }
+  if (get) {
+    // Convert the plain object from Redis into a Mongoose document
+    return User.hydrate(get);
+  }
 
   const findUser = await User.findOne({ email });
 
-  // await setUserIntoRedis(findUser);
+  await setUserIntoRedis(findUser);
 
   return findUser;
 };
